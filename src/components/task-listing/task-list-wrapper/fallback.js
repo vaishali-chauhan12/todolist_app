3ec306e5fdd6b75f07fc6e5250f4d11f7be5fdaa
vaishali-chauhan12 <vaishalichauhan.vc12@gmail.com>
2024-06-09 15:56:59 +0530

    Updated fallback section

---
+++ src/components/task-listing/task-list-wrapper/fallback.js
@@ -0,0 +1,46 @@
+
+import Button from "../../button";
+
+const FallbackSection = (props) => {
+    const {allTasks, setNewTask} = props;
+    return (
+        <div className="fallback-section">
+        <img src="../../../assets/images/no_list.jpg" />
+        <div>
+          <a
+            className="attribution-text"
+            href="https://www.freepik.com/free-vector/hand-drawn-checklist-background_3903465.htm#fromView=image_search_similar&page=2&position=48&uuid=13884f2c-a3f6-437b-9b27-ac831fee40cd"
+          >
+            Image by freepik
+          </a>
+        </div>
+        <div className="fallback-text">
+          <div className="fallback-text__line1">
+            {allTasks.length === 0
+              ? "No tasks yet"
+              : "No tasks completed yet"}
+          </div>
+
+          {allTasks.length === 0 && (
+            <div className="fallback-text__line2">
+              Add your todos and keep track of them
+            </div>
+          )}
+        </div>
+        {allTasks.length === 0 && (
+          <div className="create-button-wrapper">
+            <Button
+              type="button"
+              color="success"
+              variant="outline"
+              clickHandler={() => setNewTask(true)}
+            >
+              Add task
+            </Button>
+          </div>
+        )}
+      </div>
+    )
+}
+
+export default FallbackSection
--- src/components/task-listing/task-list-wrapper/index.jsx
+++ src/components/task-listing/task-list-wrapper/index.jsx
@@ -10,6 +10,7 @@ import { taskCollection } from "../../../store/task";
 import TaskForm from "../../task-form";
 import TaskItem from "../task-item";
 import Button from "../../button";
+import FallbackSection from "./fallback";
 
 const TaskListWrapper = (props) => {
   const [isNewTask, setNewTask] = useState(false);
@@ -116,33 +117,7 @@ const TaskListWrapper = (props) => {
         </ul>
       )}
       {!isNewTask && currrentTaskList.length === 0 && (
-        <div className="fallback-section">
-          <img src="../../../assets/images/no_list.jpg" />
-          <div>
-            <a
-              className="attribution-text"
-              href="https://www.freepik.com/free-vector/hand-drawn-checklist-background_3903465.htm#fromView=image_search_similar&page=2&position=48&uuid=13884f2c-a3f6-437b-9b27-ac831fee40cd"
-            >
-              Image by freepik
-            </a>
-          </div>
-          <div className="fallback-text">
-            <div className="fallback-text__line1">No tasks yet</div>
-            <div className="fallback-text__line2">
-              Add your todos and keep track of them
-            </div>
-          </div>
-          <div className="create-button-wrapper">
-            <Button
-              type="button"
-              color="success"
-              variant="outline"
-              clickHandler={() => setNewTask(true)}
-            >
-              Add task
-            </Button>
-          </div>
-        </div>
+        <FallbackSection allTasks={allTasks} setNewTask={setNewTask} />
       )}
     </>
   );
