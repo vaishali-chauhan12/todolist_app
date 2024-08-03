7c9e47780f7892780d2d998660d07271ce0659ce
vaishali-chauhan12 <vaishalichauhan.vc12@gmail.com>
2024-08-03 22:25:37 +0530

    Added ssl config

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
+const path = require("path");
 
-const config = {
+let config = {
   saltRounds: 10,
   db: {
     host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
-    user: process.env.DATABSE_USER,
-    password: process.env.DATABSE_PASSWORD,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
   },
 };
+if(process.env.BUILD_ENV === "PROD"){
+  const caFilePath = path.join(__dirname, "../");
+  config.db.ssl = {       
+    ca : fs.readFileSync(caFilePath + '/ca.pem'),
+  }
+}
 module.exports = config;
